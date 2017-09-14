26059a1318dbb3cdbd71c36c1508598dd8050bdb
CCIce <cck0816@gmail.com>
2017-09-14 13:28:35 +0800

    update

--- PE/PE.cpp
+++ PE/PE.cpp
@@ -1,18 +1,25 @@
 #include<stdio.h>
 #include<windows.h>
 #include<tchar.h>
+#include<iostream>
+#include<string.h>
 
-PIMAGE_DOS_HEADER m_pDosHdr=NULL; //Dos pointer
-PIMAGE_NT_HEADERS m_pNtHdr=NULL; //PE pointer
-PIMAGE_SECTION_HEADER m_pSecHdr=NULL; //section pointer
 
-LPVOID m_lpBase=NULL; //base address of image
-HANDLE m_hMap=NULL; //handle of image
-HANDLE m_hFile=NULL; //handle of file
+using namespace std;
+
+
+PIMAGE_DOS_HEADER m_pDosHdr = NULL;
+PIMAGE_NT_HEADERS m_pNtHdr = NULL;
+PIMAGE_SECTION_HEADER m_pSecHdr = NULL;
+
+LPVOID m_pbImage = NULL;
+HANDLE m_hMap = NULL;
+HANDLE m_hFile = NULL;
 
 BOOL FileCreate(const char *szFileName)
 {
-        m_hFile = CreateFile(szFileName,
+	cout << szFileName << endl;
+	HANDLE m_hFile = CreateFile(szFileName,
 		GENERIC_READ,
 		FILE_SHARE_READ,
 		NULL,
@@ -23,7 +30,7 @@ BOOL FileCreate(const char *szFileName)
 	if (m_hFile == INVALID_HANDLE_VALUE)
 	{
 
-	        printf("error open file...\r\n");
+		printf("error open file...\r\n");
 		return FALSE;
 	}
 
@@ -35,10 +42,10 @@ BOOL FileCreate(const char *szFileName)
 		return FALSE;
 	}
 
-	m_lpBase = MapViewOfFile(m_hMap, FILE_MAP_READ, 0, 0, 0);
-	if (m_lpBase == NULL)
+	m_pbImage = MapViewOfFile(m_hMap, FILE_MAP_READ, 0, 0, 0);
+	if (m_pbImage == NULL)
 	{
-	    printf("ipBase is NULL");
+		printf("ipBase is NULL");
 		CloseHandle(m_hMap);
 		CloseHandle(m_hFile);
 		return FALSE;
@@ -48,19 +55,21 @@ BOOL FileCreate(const char *szFileName)
 
 BOOL IsPeFileAndGetPePoint()
 {
-	m_pDosHdr = (PIMAGE_DOS_HEADER)m_lpBase;
+
+	//	LPVOID m_lpBase;
+	//	PIMAGE_SECTION_HEADER m_pSecHdr;
+	m_pDosHdr = (PIMAGE_DOS_HEADER)m_pbImage;
 
 	if (m_pDosHdr->e_magic != IMAGE_DOS_SIGNATURE)
 	{
-	    printf("Not MZ header\n");
+		printf("Not MZ header\n");
 		return FALSE;
 	}
 
-	m_pNtHdr = (PIMAGE_NT_HEADERS)((DWORD)m_lpBase + m_pDosHdr->e_lfanew);
+	m_pNtHdr = (PIMAGE_NT_HEADERS)((DWORD)m_pbImage + m_pDosHdr->e_lfanew);
 
 	if (m_pNtHdr->Signature != IMAGE_NT_SIGNATURE)
 	{
-	    printf("Open False");
 		return FALSE;
 	}
 
@@ -68,7 +77,6 @@ BOOL IsPeFileAndGetPePoint()
 	return TRUE;
 }
 
-//print information of PE
 VOID CPeParseDig()
 {
 	printf("address of EntryPoint:");
@@ -84,18 +92,17 @@ VOID CPeParseDig()
 	printf("%02X\n", m_pNtHdr->FileHeader.NumberOfSections);
 
 	printf("size of code:");
-	printf ("%08x\n", m_pNtHdr->OptionalHeader.SizeOfCode);
+	printf("%08x\n", m_pNtHdr->OptionalHeader.SizeOfCode);
 
 }
 
-//Print information of sections
 VOID EnumSections()
 {
 	int nSecNum = m_pNtHdr->FileHeader.NumberOfSections;
 
 	for (int i = 0; i < nSecNum; i++)
 	{
-	    printf("Section:%d\n", i+1);
+		printf("Section:%d\n", i + 1);
 		printf("%08X\n", m_pSecHdr[i].VirtualAddress);
 		printf("%08X\n", m_pSecHdr[i].Misc.VirtualSize);
 		printf("%08X\n", m_pSecHdr[i].PointerToRawData);
@@ -105,27 +112,85 @@ VOID EnumSections()
 	}
 }
 
+DWORD OffsetToRva(DWORD dwOffset, LPVOID m_pbImage)
+{
+    DWORD dwSecBorder = -1;
+
+    PIMAGE_NT_HEADERS m_pNtHdr = PIMAGE_NT_HEADERS((DWORD)m_pbImage + (DWORD)PIMAGE_DOS_HEADER(m_pbImage)->e_lfanew);
+    PIMAGE_SECTION_HEADER m_pSecHdr = PIMAGE_SECTION_HEADER(PBYTE(m_pNtHdr) + sizeof(IMAGE_NT_HEADERS));
+
+    for(DWORD i = 0; i < m_pNtHdr->FileHeader.NumberOfSections; i++)
+    {
+        if(dwOffset >= m_pSecHdr[i].PointerToRawData && dwOffset < m_pSecHdr[i].PointerToRawData + m_pSecHdr[i].SizeOfRawData)
+            return  dwOffset - m_pSecHdr[i].PointerToRawData + m_pSecHdr[i].VirtualAddress;
+
+        if(m_pSecHdr[i].PointerToRawData && m_pSecHdr[i].PointerToRawData < dwSecBorder)
+            dwSecBorder = m_pSecHdr[i].PointerToRawData;
+    }
+
+    if(dwOffset < dwSecBorder)
+        return dwOffset;
+    else
+        return NULL;
+
+
+}
+
+LPVOID RvaToPointer(DWORD dwRva, LPVOID m_pbImage)
+{
+    DWORD dwSecBorder = -1;
+    PIMAGE_NT_HEADERS m_pNtHdr = PIMAGE_NT_HEADERS((DWORD)m_pbImage + PIMAGE_DOS_HEADER(m_pbImage)->e_lfanew);
+    PIMAGE_SECTION_HEADER m_pSecHdr = PIMAGE_SECTION_HEADER((PBYTE)m_pNtHdr + sizeof(PIMAGE_NT_HEADERS));
+
+    for(int i = 0; i < m_pNtHdr->FileHeader.NumberOfSections; i++)
+    {
+        if(dwRva >= m_pSecHdr[i].VirtualAddress && dwRva < m_pSecHdr[i].VirtualAddress + m_pSecHdr[i].Misc.VirtualSize)
+        {
+            return (m_pbImage + (dwRva - m_pSecHdr[i].VirtualAddress + m_pSecHdr[i].PointerToRawData));
+        }
+
+        if(m_pSecHdr[i].PointerToRawData && m_pSecHdr[i].PointerToRawData < dwSecBorder)
+            dwSecBorder = m_pSecHdr[i].PointerToRawData;
+    }
+
+    if(dwRva < dwSecBorder)
+        return m_pbImage + dwRva;
+    else
+        return NULL;
+}
+
+
 
 VOID FreePoint()
 {
 	CloseHandle(m_hMap);
 	CloseHandle(m_hFile);
+	CloseHandle(m_pbImage);
+
+	free(m_pDosHdr);
+	free(m_pNtHdr);
+	free(m_pSecHdr);
 }
 
 int main()
 {
-	const char* szFileName = "c:\\windows\\system32\\cmd.exe";
-	if (FileCreate(szFileName))
-	{
+	//    "D:\\The Sims 4\\Game\\Bin\\TS4_x64.exe";
+	const char* szFileName = "C:\\Users\\Administrator\\Desktop\\add.exe";
+//	if (FileCreate(szFileName))
+//	{
+//
+//		if (IsPeFileAndGetPePoint())
+//		{
+//			CPeParseDig();
+//			EnumSections();
+//			FreePoint();
+//		}
+//	}
 
-		if (IsPeFileAndGetPePoint())
-		{
-			CPeParseDig();
-			EnumSections();
-			FreePoint();
-		}
-	}
+    FileCreate(szFileName);
+//    LPVOID a = RvaToPointer((DWORD)0x002288, m_pbImage);
+    DWORD a = OffsetToRva((DWORD)0x0011, m_pbImage);
+    cout << a << endl;
 
-    FreePoint();
 	return 0;
 }
--- PE/readme.md
+++ PE/readme.md
@@ -1,1 +1,5 @@
+=========2017-9-17 Update========
+Add RvaToPointer function
+Add OffsetToRva function
+
 Simple PE Loader Tool
